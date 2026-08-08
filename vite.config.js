924a4444fc8492ded57f4fe8b74e362c2b091369
NourhanFareed <nourhanfarid883@gmail.com>
2026-08-08 04:59:52 +0300

    5

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {},
-  resolve: {},
-  base: "/Yummies/", // replace with your repo name
+  resolve: {}, 
 });
